5797dd385f629c876871796d70acaa9fb9567c99
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2021-04-24 15:28:55 +0200

    Experimenting markdown links

--- client/src/PreviewComponent.js
+++ client/src/PreviewComponent.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReactMarkdown from 'react-markdown'
+import { uriTransformer } from 'react-markdown'
 
 export default class PreviewComponent extends React.PureComponent {
     constructor(props) {
         super(props)
+
+        this.transformImageUri = this.transformImageUri.bind(this)
     }
 
     componentDidMount() {
@@ -13,8 +16,12 @@ export default class PreviewComponent extends React.PureComponent {
     componentWillUnmount() {
     }
 
+    transformImageUri(src, alt, title) {
+        return uriTransformer(src, alt, title)
+    }
+
     render() {
-        return <ReactMarkdown className="markdown-body">{this.props.markdown}</ReactMarkdown>
+        return <ReactMarkdown className="markdown-body" transformLinkUri={uriTransformer} transformImageUri={this.transformImageUri}>{this.props.markdown}</ReactMarkdown>
     }
 }
 
